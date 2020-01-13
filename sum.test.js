9db6cc068e6e8df3c457a81a621a0bad04e9e1b0
Kyle Hicks <kylehicks@users-mbp.lan>
2020-01-13 14:13:40 -0500

    adding bad test to test CI

--- sum.test.js
+++ sum.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 2 + 2 to equal 4', () => {
-  expect(sum(2, 2)).toBe(4);
+test('BAD TEST: adds 2 + 3 to equal 4', () => {
+  expect(sum(2, 3)).toBe(4);
 });
